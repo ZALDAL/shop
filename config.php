3380a4fd39fc0c0a8bbdbfde4f42f92676ae3b4f
Le Nguyen Duc Tai <lenguyenductai208@gmail.com>
2025-07-27 12:48:03 +0700

    kết nối với databsae

---
+++ config.php
@@ -0,0 +1,35 @@
+File config.php
+<?php
+// APPLICATION
+define('APPLICATION', 'Catalog');
+
+// HTTP
+define('HTTP_SERVER', 'https://shop1-116b421cb3ab.herokuapp.com/'); // <-- sửa lại theo domain thật
+
+// DIR
+define('DIR_OPENCART', '/app/'); // <-- sửa lại theo đường dẫn thật
+define('DIR_APPLICATION', DIR_OPENCART . 'catalog/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_STORAGE', DIR_OPENCART . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'kokwqc.stackhero-network.com');
+define('DB_USERNAME', 'zaldal');
+define('DB_PASSWORD', 'ABC@123');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '35411');
+define('DB_PREFIX', 'oc_');
+define('DB_SSL_KEY', '');
+define('DB_SSL_CERT', '');
+define('DB_SSL_CA', '');
